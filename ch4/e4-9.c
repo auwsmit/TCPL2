f982ffe4bb047588d90261ea9125af6026736ab3
Austin Smith <AssailantLF@gmail.com>
2016-03-22 10:21:33 -0700

    updated 4-11 & corrected 4-9

--- ch4/e4-9.c
+++ ch4/e4-9.c
@@ -1,18 +1,19 @@
-/* Exercise; Our getch and ungetch do not handle a pushed-back EOF correctly.
+/*
+ * Exercise: Our getch and ungetch do not handle a pushed-back EOF correctly.
  * Decide what their properties ought to be if an EOF is pushed back, then
- * implement your design. */
+ * implement your design.
+ *
+ * Answer: My implementation is to ignore EOF when it is pushed back.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>     /* for atof() */
 #include <ctype.h>
 
-#define MAXOP     100   /* max size of operand or operator */
-#define NUMBER    '0'   /* signal that a number was found */
-#define EOF_FOUND '1'   /* signal that EOF was found */
-#define EOT       4     /* value for "end of transmission" character */
-#define SUB       26    /* value for "substitute" character */
-#define BUFSIZE   100
-#define MAXVAL    100   /* maximum depth of val stack */
+#define MAXOP    100    /* max size of operand or operator */
+#define NUMBER   '0'    /* signal that a number was found */
+#define BUFSIZE  100
+#define MAXVAL   100    /* maximum depth of val stack */
 
 char buf[BUFSIZE];      /* buffer for ungetch */
 int bufp = 0;           /* next free position in buf */
@@ -22,8 +23,7 @@ double val[MAXVAL];     /* value stack */
 /* get a (possibly pushed back) character */
 int getch(void)
 {
-    int r = (bufp > 0) ? buf[--bufp] : getchar();
-    return (r == EOT || r == SUB) ? -1 : r;
+    return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
 /* push character back on input */
@@ -31,8 +31,8 @@ void ungetch(int c)
 {
     if (bufp >= BUFSIZE)
         printf("ungetch: too many characters\n");
-    else
-        buf[bufp] = c;
+    else if (c != EOF)
+        buf[bufp++] = c;
 }
 
 /* getop: get next operator or numeric operand */
@@ -43,9 +43,8 @@ int getop(char s[])
     while ((s[0] = c = getch()) == ' ' || c == '\t')
         ;
     s[1] = '\0';
-    if (!isdigit(c) && c != '.') {
+    if (!isdigit(c) && c != '.')
         return c;       /* not a number */
-    }
     i = 0;
     if (isdigit(c))     /* collect integer part */
         while (isdigit(s[++i] = c = getch()))
@@ -86,6 +85,9 @@ int main(void)
     double op2;
     char s[MAXOP];
 
+    ungetch(' ');
+    ungetch(EOF);
+
     while ((type = getop(s)) != EOF) {
         switch(type) {
         case NUMBER:
@@ -108,11 +110,18 @@ int main(void)
             else
                 printf("error: no zero divisor\n");
             break;
+        case '%':
+            op2 = pop();
+            if (op2 != 0.0)
+                push((int)pop() % (int)op2);
+            else
+                printf("error: no zero divisor\n");
+            break;
         case '\n':
             printf("\t%.8g\n", pop());
             break;
         default:
-            printf("error: unknown command %d [%c]\n", s[0], s[0]);
+            printf("error: unknown command %s\n", s);
             break;
         }
     }
