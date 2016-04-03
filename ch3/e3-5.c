f76bc5646cd7fa0bf4aa6d33b9f150a3a26e87f9
Austin Smith <AssailantLF@gmail.com>
2016-04-02 17:10:20 -0700

    removed an unnecessary function

--- ch3/e3-5.c
+++ ch3/e3-5.c
@@ -20,22 +20,6 @@ void reverse(char s[])
     }
 }
 
-void itoa(int n, char s[])
-{
-    int i, sign;
-
-    if ((sign = n) < 0)
-        n = -n;
-    i = 0;
-    do {
-        s[i++] = n % 10 + '0';
-    } while ((n /= 10) > 0);
-    if (sign < 0)
-        s[i++] = '-';
-    s[i] = '\0';
-    reverse(s);
-}
-
 void itob(int n, char s[], int base)
 {
     int i, sign;
